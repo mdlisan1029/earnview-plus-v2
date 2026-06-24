95d76d211164dd1df6fd874c14c3c7874ba6fccc
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 12:23:57 +0600

    Refactor login and offer management functions

--- admin/script.js
+++ admin/script.js
@@ -1,68 +1,261 @@
-async function login(){
-
-const username =
-document.getElementById("username").value;
-
-const password =
-document.getElementById("password").value;
+const API = 'https://YOUR_RENDER_URL.onrender.com';
 
 
-const response = await fetch(
 
-"https://YOUR_RENDER_URL.onrender.com/api/admin/login",
-
-{
-
-method:"POST",
-
-headers:{
-
-"Content-Type":"application/json"
-
-},
-
-body:JSON.stringify({
-
-username,
-
-password
-
-})
-
-}
-
-);
+async function addOffer(){
 
 
-const data = await response.json();
+const formData = new FormData();
 
 
-if(data.success){
 
-localStorage.setItem(
+formData.append(
 
-"adminToken",
-
-data.token
-
-);
-
-window.location.href="index.html";
-
-}
-
-
-else{
+'title',
 
 document.getElementById(
 
-"msg"
+'title'
 
-).innerText=
+).value
 
-data.message;
+);
+
+
+
+formData.append(
+
+'description',
+
+document.getElementById(
+
+'description'
+
+).value
+
+);
+
+
+
+formData.append(
+
+'reward',
+
+document.getElementById(
+
+'reward'
+
+).value
+
+);
+
+
+
+formData.append(
+
+'link',
+
+document.getElementById(
+
+'link'
+
+).value
+
+);
+
+
+
+formData.append(
+
+'category',
+
+document.getElementById(
+
+'category'
+
+).value
+
+);
+
+
+
+formData.append(
+
+'image',
+
+document.getElementById(
+
+'image'
+
+).files[0]
+
+);
+
+
+
+
+await fetch(
+
+API+
+
+'/api/offers/add',
+
+{
+
+method:'POST',
+
+body:formData
+
+}
+
+);
+
+
+
+loadOffers();
+
+
 
 }
 
 
+
+
+
+async function loadOffers(){
+
+
+const res = await fetch(
+
+API+
+
+'/api/offers'
+
+);
+
+
+
+const offers = await res.json();
+
+
+
+let html='';
+
+
+
+offers.forEach(o=>{
+
+
+html+=`
+
+
+<div class="card">
+
+
+<h3>
+
+${o.title}
+
+</h3>
+
+
+<p>
+
+${o.reward} BDT
+
+</p>
+
+
+
+<img
+
+
+src="${API}/uploads/${o.image}"
+
+
+width="100"
+
+
+>
+
+
+
+<br>
+
+
+<button
+
+
+onclick="deleteOffer(
+
+${o.id}
+
+)"
+
+
+>
+
+
+Delete
+
+
+</button>
+
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+
+document.getElementById(
+
+'offers'
+
+).innerHTML=html;
+
+
+
 }
+
+
+
+
+async function deleteOffer(id){
+
+
+await fetch(
+
+
+API+
+
+'/api/offers/'+id,
+
+
+{
+
+
+method:'DELETE'
+
+
+}
+
+
+);
+
+
+
+loadOffers();
+
+
+
+}
+
+
+
+loadOffers();
