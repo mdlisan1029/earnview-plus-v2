7fda108a92699b41bc995e71046665986b30714a
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 10:53:48 +0600

    Add admin login route

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,1 +1,20 @@
+const express=require('express')
 
+const router=express.Router()
+
+
+const adminController=require('../controllers/adminController')
+
+
+
+router.post(
+
+'/login',
+
+adminController.login
+
+)
+
+
+
+module.exports=router
