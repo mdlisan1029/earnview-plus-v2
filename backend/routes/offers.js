11424f0e5dc9604580037ff42c96bcf285707669
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 12:03:09 +0600

    Create offer routes

--- backend/routes/offers.js
+++ backend/routes/offers.js
@@ -1,1 +1,39 @@
+const express = require('express');
 
+const router = express.Router();
+
+const offerController = require('../controllers/offerController');
+
+
+
+router.post(
+
+'/add',
+
+offerController.addOffer
+
+);
+
+
+
+router.get(
+
+'/',
+
+offerController.getOffers
+
+);
+
+
+
+router.delete(
+
+'/:id',
+
+offerController.deleteOffer
+
+);
+
+
+
+module.exports = router;
