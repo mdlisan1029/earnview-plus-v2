c4c4fbd6a9ad60727fe04a6fe6dc78322afaa951
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 16:42:42 +0600

    Create auth routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,23 @@
+const express=require('express');
 
+const router=express.Router();
+
+const authController=require(
+
+'../controllers/authController'
+
+);
+
+
+
+router.post(
+
+'/telegram',
+
+authController.telegramLogin
+
+);
+
+
+
+module.exports=router;
