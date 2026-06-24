205aff7c1c04d6e0fe2d2897446b5e8a86515b52
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 17:25:10 +0600

    Add new user object if not exists in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,37 @@
+if(!user){
 
+user={
+
+id:Date.now(),
+
+telegram_id,
+
+username,
+
+balance:0,
+
+created_at:new Date(),
+
+referralCode:Date.now().toString(),
+
+referredBy:null,
+
+referrals:0
+
+};
+
+
+db.users.push(
+
+user
+
+);
+
+
+writeDB(
+
+db
+
+);
+
+}
