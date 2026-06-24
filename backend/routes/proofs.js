356f3511e8ab6b8e8dc8f9818dd71e7c864ab1b9
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 12:58:53 +0600

    Add routes for submitting and retrieving proofs

--- backend/routes/proofs.js
+++ backend/routes/proofs.js
@@ -1,1 +1,37 @@
+const express = require('express');
 
+const router = express.Router();
+
+const upload = require('../config/upload');
+
+const proofController = require('../controllers/proofController');
+
+
+
+router.post(
+
+'/submit',
+
+upload.single(
+
+'image'
+
+),
+
+proofController.submitProof
+
+);
+
+
+
+router.get(
+
+'/',
+
+proofController.getProofs
+
+);
+
+
+
+module.exports = router;
