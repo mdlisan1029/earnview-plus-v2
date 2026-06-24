4f56988d5933842ba3c09f0204211a88db251f0b
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 11:24:01 +0600

    Implement async login function for admin

--- admin/script.js
+++ admin/script.js
@@ -1,1 +1,68 @@
+async function login(){
 
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+
+const response = await fetch(
+
+"https://YOUR_RENDER_URL.onrender.com/api/admin/login",
+
+{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":"application/json"
+
+},
+
+body:JSON.stringify({
+
+username,
+
+password
+
+})
+
+}
+
+);
+
+
+const data = await response.json();
+
+
+if(data.success){
+
+localStorage.setItem(
+
+"adminToken",
+
+data.token
+
+);
+
+window.location.href="index.html";
+
+}
+
+
+else{
+
+document.getElementById(
+
+"msg"
+
+).innerText=
+
+data.message;
+
+}
+
+
+}
