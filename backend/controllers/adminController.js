ba4b167bd75d83f45da210ffd90a5976298d8b0a
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 10:46:39 +0600

    Add login function for admin authentication

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,1 +1,39 @@
+exports.login = (req,res)=>{
 
+
+const {username,password}=req.body;
+
+
+if(
+
+username==="admin"
+
+&&
+
+password==="123456"
+
+)
+
+{
+
+return res.json({
+
+success:true,
+
+token:"earnview_admin_2026"
+
+})
+
+}
+
+
+return res.status(401).json({
+
+success:false,
+
+message:"Wrong username or password"
+
+})
+
+
+}
