c9e88530c696588d09ee5b48ecacb44dc536bb46
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 19:09:38 +0600

    Add analytics route for fetching stats

---
+++ backend/routes/analytics.js
@@ -0,0 +1,23 @@
+const express=require('express');
+
+const router=express.Router();
+
+const analyticsController=require(
+
+'../controllers/analyticsController'
+
+);
+
+
+
+router.get(
+
+'/stats',
+
+analyticsController.getStats
+
+);
+
+
+
+module.exports=router;
