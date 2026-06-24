7b71fb78a9de0431899d940184bf57319137043f
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 12:52:47 +0600

    Add Proof class with constructor and properties

--- backend/models/proof.js
+++ backend/models/proof.js
@@ -1,1 +1,33 @@
+class Proof{
 
+constructor(
+
+id,
+
+userId,
+
+offerId,
+
+image,
+
+status
+
+){
+
+this.id=id;
+
+this.userId=userId;
+
+this.offerId=offerId;
+
+this.image=image;
+
+this.status=status;
+
+}
+
+
+}
+
+
+module.exports=Proof;
