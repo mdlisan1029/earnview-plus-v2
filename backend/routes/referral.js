5894cd35102593c06963113d00fb937240f431f2
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 17:13:34 +0600

    Add referral route to handle GET requests

--- backend/routes/referral.js
+++ backend/routes/referral.js
@@ -1,1 +1,21 @@
+const express=require('express');
 
+const router=express.Router();
+
+const referralController=require(
+
+'../controllers/referralController'
+
+);
+
+
+router.get(
+
+'/:id',
+
+referralController.getReferral
+
+);
+
+
+module.exports=router;
