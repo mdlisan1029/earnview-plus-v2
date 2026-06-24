3441681f06eb1c81c0eb28438f7d703120b7104e
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 16:00:15 +0600

    Create withdraw routes

--- backend/routes/withdraw.js
+++ backend/routes/withdraw.js
@@ -1,1 +1,44 @@
+const express = require('express');
 
+const router = express.Router();
+
+const withdrawController = require('../controllers/withdrawController');
+
+
+router.post(
+
+'/create',
+
+withdrawController.createWithdraw
+
+);
+
+
+router.get(
+
+'/',
+
+withdrawController.getWithdraws
+
+);
+
+
+router.post(
+
+'/approve/:id',
+
+withdrawController.approveWithdraw
+
+);
+
+
+router.post(
+
+'/reject/:id',
+
+withdrawController.rejectWithdraw
+
+);
+
+
+module.exports = router;
