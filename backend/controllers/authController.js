f6789c06eddc4fbf065a89b1ba987f822a60f4a2
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 18:41:43 +0600

    Implement Telegram login with user referral handling

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,86 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.join(
+__dirname,
+'../database.json'
+);
+
+
+function readDB(){
+
+return JSON.parse(
+
+fs.readFileSync(
+
+dbPath
+
+)
+
+);
+
+}
+
+
+
+function writeDB(data){
+
+fs.writeFileSync(
+
+dbPath,
+
+JSON.stringify(
+
+data,
+
+null,
+
+2
+
+)
+
+);
+
+}
+
+
+
+exports.telegramLogin=(req,res)=>{
+
+
+const {
+
+telegram_id,
+
+username,
+
+start_param
+
+}=req.body;
+
+
+
+const db=readDB();
+
+
+
+let user=db.users.find(
+
+u=>u.telegram_id==telegram_id
+
+);
+
+
+
 if(!user){
 
-let referredBy = null;
+
+let referredBy=null;
+
+
 
 if(start_param){
+
 
 const referrer=db.users.find(
 
@@ -11,35 +89,52 @@ u=>u.referralCode===start_param
 );
 
 
+
 if(referrer){
+
 
 referredBy=referrer.id;
 
+
+
 referrer.referrals+=1;
 
-}
 
 }
+
+
+
+}
+
 
 
 
 user={
 
+
 id:Date.now(),
+
 
 telegram_id,
 
+
 username,
+
 
 balance:0,
 
+
 created_at:new Date(),
+
 
 referralCode:Date.now().toString(),
 
+
 referredBy,
 
+
 referrals:0
+
 
 };
 
@@ -59,4 +154,22 @@ db
 
 );
 
+
 }
+
+
+
+
+res.json({
+
+
+success:true,
+
+
+user
+
+
+});
+
+
+};
