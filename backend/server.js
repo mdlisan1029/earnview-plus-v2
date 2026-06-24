1c5352d67a6d22fa2f6aa6b5bf73acb0d3a2e478
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 11:56:19 +0600

    Connect offer routes

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,34 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 
+const offerRoutes = require('./routes/offers');
+
+
 app.use(express.json());
+
+
 
 app.use('/api/admin', adminRoutes);
 
+app.use('/api/offers', offerRoutes);
+
+
+
+
 app.get('/', (req,res)=>{
-    res.send('EarnView Plus Backend Running');
+
+res.send('EarnView Plus Backend Running');
+
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 
+
+
 app.listen(PORT,()=>{
-    console.log(`Server running on ${PORT}`);
+
+console.log(`Server running on ${PORT}`);
+
 });
