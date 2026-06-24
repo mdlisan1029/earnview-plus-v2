c424de4b4c444a90ceb1ffc3f452de2116133f4e
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 12:19:28 +0600

    Enable image upload in offers route

--- backend/routes/offers.js
+++ backend/routes/offers.js
@@ -4,11 +4,19 @@ const router = express.Router();
 
 const offerController = require('../controllers/offerController');
 
+const upload = require('../config/upload');
+
 
 
 router.post(
 
 '/add',
+
+upload.single(
+
+'image'
+
+),
 
 offerController.addOffer
 
