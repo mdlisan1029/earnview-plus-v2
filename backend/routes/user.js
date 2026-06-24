7e2591fa1e18306410dfb3dcb563a795774019d7
mdlisan1029 <lisansarker002@gmail.com>
2026-06-24 17:55:23 +0600

    Add user route to get user by ID

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,1 +1,23 @@
+const express=require('express');
 
+const router=express.Router();
+
+const userController=require(
+
+'../controllers/userController'
+
+);
+
+
+
+router.get(
+
+'/:id',
+
+userController.getUser
+
+);
+
+
+
+module.exports=router;
